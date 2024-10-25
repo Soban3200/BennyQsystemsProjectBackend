44050fab5371b2d6070bd0d856aa2861471951c9
Soban3200 <159254628+Soban3200@users.noreply.github.com>
2024-10-25 13:38:28 +0530

    Update connectDatabase.js

--- config/connectDatabase.js
+++ config/connectDatabase.js
@@ -4,7 +4,7 @@ const connectDatabase = () => {
     mongoose.connect(process.env.DB_URL, {
         useNewUrlParser: true,  // Use the new URL parser
         useUnifiedTopology: true, // Use the new topology engine
-        poolSize: 10 // Set the connection pool size
+        // No need to specify poolSize here; instead, set it on the mongoose global
     })
     .then((con) => {
         console.log('MongoDB connected to host: ' + con.connection.host);
@@ -14,5 +14,8 @@ const connectDatabase = () => {
         process.exit(1); // Exit the process if there's a connection error
     });
 };
+
+// Set the connection pool size globally for Mongoose
+mongoose.set('poolSize', 10);
 
 module.exports = connectDatabase;
