4acebd743a0c1036d99a289336c7758352ab8d52
Soban3200 <159254628+Soban3200@users.noreply.github.com>
2024-10-25 13:33:40 +0530

    Update connectDatabase.js

--- config/connectDatabase.js
+++ config/connectDatabase.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
-const connectDatabase=() => {
-    mongoose.connect(process.env.DB_URL).then((con)=>{
-        console.log('MongoDB connected to host'+con.connection.host);
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_URL, {
+        useNewUrlParser: true,  // Use the new URL parser
+        useUnifiedTopology: true, // Use the new topology engine
+        poolSize: 10 // Set the connection pool size
     })
-
+    .then((con) => {
+        console.log('MongoDB connected to host: ' + con.connection.host);
+    })
+    .catch((error) => {
+        console.error('MongoDB connection error: ', error);
+        process.exit(1); // Exit the process if there's a connection error
+    });
 };
 
-module.exports = connectDatabase
+module.exports = connectDatabase;
